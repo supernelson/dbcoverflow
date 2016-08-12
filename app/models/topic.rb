821c2bc1d981aa329f7b63ce8654d1ca688a02a8
Nelson Chen <nelsonchen1020@gmail.com>
2016-08-12 15:56:31 -0700

    Fixed all migrations and seeds

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,6 +2,7 @@ class Topic < ActiveRecord::Base
   # Remember to create a migration!
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
+  has_many :answers
 
   belongs_to :user
 
