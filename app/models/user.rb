d9e98ccc207125073a0021873d73e651ba316eaa
David Ross <rustyross101@gmail.com>
2016-08-12 15:10:20 -0700

    solves merge conflicts

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+  
+  # Remember to create a migration!
+  has_many :topics, :answers, :votes, :comments
+
+  validates :name, presence: true
+  validates :username, presence: true
+  validates :password, presence: true
+
+
+  
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+
+  def authenticate(user_password)
+  	self.password == user_password
+  end
+end
+	
